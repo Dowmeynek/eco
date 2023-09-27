417cb08c850052123b30dbb40cbe8536808b0fb2
Dowmeynek <106123479+Dowmeynek@users.noreply.github.com>
2023-09-27 20:03:16 +0800

    main

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/main', 'Home::index');
+$routes->get('/ulo', 'Home::ulo');
